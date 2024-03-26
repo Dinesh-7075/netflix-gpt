4062b31cb8278f993320de0ad27970660a9f8616
Dinesh-7075 <jammidinesh717@gmail.com>
2024-03-26 14:15:53 +0530

    changes in const file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,13 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const AVATAR_URL = "https://occ-0-2701-3663.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
 
+const TMDB_API_KEY = process.env.REACT_APP_TMDB_KEY;
+
 export const API_OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization:  "Bearer " + process.env.REACT_APP_TMDB_KEY,
+      Authorization:  "Bearer " + TMDB_API_KEY,
     }
   };
 
